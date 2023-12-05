1a4e188e107dacc4fc867cb38a43bf7a2b690166
maleblanc <maleblanc55@gmail.com>
2023-12-04 22:54:16 -0500

    Adding installation scrip for extensions

---
+++ install_vscode_extension.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension ms-vscode.cmake-tools
+code --install-extension mhutchie.git-graph
+code --install-extension vsciot-vscode.vscode-arduino
+code --install-extension qcz.text-power-tools
+code --install-extension jebbs.plantuml
+code --install-extension ms-vscode.cpptools
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension ms-vscode.vscode-serial-monitor
+code --install-extension tomoki1207.pdf
