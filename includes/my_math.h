2f59e0433633a04885932ef00b71f1d1b573636f
maleblanc <maleblanc55@gmail.com>
2023-12-11 22:17:53 -0500

    Removing test code

--- includes/my_math.h
+++
@@ -1,7 +0,0 @@
-// File: my_math.h
-#ifndef MY_MATH_H
-#define MY_MATH_H
-
-int add(int a, int b);
-
-#endif // MY_MATH_H
--- src/my_math.cpp
+++
@@ -1,6 +0,0 @@
-// File: my_math.cpp
-#include "../includes/my_math.h"
-
-int add(int a, int b) {
-    return a + b;
-}
